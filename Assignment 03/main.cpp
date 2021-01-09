312130a4aa208e88d9788718a5847ad3e0db5cf2
Charlie-Say <charliesay1990@hotmail.com>
2021-01-08 21:27:11 -0800

    final edit

--- Assignment 03/main.cpp
+++ Assignment 03/main.cpp
@@ -15,63 +15,55 @@
 #include <string>
 using namespace std;
 
-struct node
-{
+struct node {
     int data;
     struct node *next;
 };
 
-class linked_list
-{
+class linked_list {
+
 private:
     node *head,*tail;
+
 public:
-    linked_list()
-    {
+    linked_list() {
         head = NULL;
         tail = NULL;
     }
 
 /* Add a value to BACK of the queue */
-    void enqueue(int n)
-    {
+    void enqueue(int n) {
         node *tmp = new node;
         tmp->data = n;
         tmp->next = NULL;
 
-        if(head == NULL)
-        {
+        if(head == NULL) {
             head = tmp;
             tail = tmp;
         }
-        else
-        {
+
+        else {
             tail->next = tmp;
             tail = tail->next;
         }
     }
 
-    node* gethead()
-    {
+    node* gethead() {
         return head;
     }
 
-    static void display(node *head)
-    {
-        if(head == NULL)
-        {
+    static void display(node *head) {
+        if(head == NULL) {
             cout << "NULL" << endl;
         }
-        else
-        {
+        else {
             cout << head->data << endl;
             display(head->next);
         }
     }
 
 /* Remove element from FRONT of the queue and return its value */
-    int dequeue()
-    {
+    int dequeue() {
         node* temp;
         temp = head;
         head = head->next;
@@ -82,42 +74,38 @@ public:
 
 
 /* Driver code */ 
-int main() 
-{
+int main() {
+
     int data;
     int counter = 5;
     int inputCounter = 3;
     string yesNo;
     linked_list randomList;
 
-    while (true)
-    {
+    while (true) {
         cout << "Add a value:  ";
         cin >> data;
         randomList.enqueue(data);
         counter--;
 
-        if (counter == 0)
-        {
+        if (counter == 0) {
             break;
         }
     }
 
-    while (true)
-    {
+    while (true) {
         cout << "Remove value from front of queue. PRESS Y: ";
         cin >> yesNo;
-        if (yesNo == "y")
-        {
+        if (yesNo == "y") {
             randomList.dequeue();
             linked_list::display(randomList.gethead());
             inputCounter--;
+
         } else {
             linked_list::display(randomList.gethead());
         }
 
-        if (inputCounter == 0)
-        {
+        if (inputCounter == 0) {
             break;
         }
     }
