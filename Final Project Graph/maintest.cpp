b9f1c1d6cf8211201e8211d56e8100e6042f17ba
Charlie-Say <charliesay1990@hotmail.com>
2021-02-22 01:56:33 -0800

    test file

---
+++ Final Project Graph/maintest.cpp
@@ -0,0 +1,46 @@
+/*
+
+1. Create some tests before you start coding...
+
+2. Implement a graph class with at least:
+
+    - a function to add a new vertex to the graph (perhaps add_vertex(vertex_name))
+
+    - a function to add a new edge between two vertices of the graph (perhaps add_edge(source, destination) or source.add_edge(destination))
+
+    - a function for a shortest path algorithm (perhaps shortest_path(source, destination))
+
+    - a function for a minimum spanning tree algorithm (example min_span_tree()).
+
+3. Analyze the complexity of all of your graph behaviors
+
+
+https://www.geeksforgeeks.org/graph-and-its-representations/
+
+
+has weighted graph that might help with shortest path algorithm
+https://www.techiedelight.com/graph-implementation-using-stl/
+
+minimum spanning tree algo
+https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
+
+*/
+
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include<list>
+using namespace std;
+
+
+class graphData {
+
+}
+
+
+
+
+int main() {
+    return 0;
+}
